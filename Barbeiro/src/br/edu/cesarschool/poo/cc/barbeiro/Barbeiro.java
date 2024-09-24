56b128ab8823bccf76d2ed9f57d5237f06b25254
Becker <142417785+MiguelBecker@users.noreply.github.com>
2024-09-24 19:28:30 -0300

    final

--- Barbeiro/src/br/edu/cesarschool/poo/cc/barbeiro/Barbeiro.java
+++ Barbeiro/src/br/edu/cesarschool/poo/cc/barbeiro/Barbeiro.java
@@ -4,29 +4,44 @@ import java.util.Random;
 
 public class Barbeiro extends Pessoa implements Runnable {
     private Barbearia barbearia;
+    private boolean dormindo;
 
     public Barbeiro(int id, Barbearia barbearia) {
         super(id);
         this.barbearia = barbearia;
+        this.dormindo = false;
     }
+
     @Override
     public void run() {
         while (true) {
-            Cliente cliente = barbearia.proximoCliente();
-            if (cliente != null) {
-                System.out.println("Cliente " + cliente.getID() + " cortando cabelo com Barbeiro " + getID());
-                try {
-                    Thread.sleep(new Random().nextInt(2000) + 1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+            synchronized (barbearia) {
+                while (barbearia.filaVazia()) {
+                    if (!dormindo) {
+                        System.out.println("Barbeiro " + getID() + " indo dormir um pouco... não há clientes na barbearia...");
+                        dormindo = true;
+                    }
+                    try {
+                        barbearia.wait(); 
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                 }
-                barbearia.corteTerminado(cliente);
-            } else {
-                System.out.println("Barbeiro " + getID() + " indo dormir um pouco... não há clientes na barbearia...");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
+
+                if (dormindo) {
+                    System.out.println("Barbeiro " + getID() + " acordou! Começando os trabalhos!");
+                    dormindo = false;
+                }
+
+                Cliente cliente = barbearia.proximoCliente();
+                if (cliente != null) {
+                    System.out.println("Cliente " + cliente.getID() + " cortando cabelo com Barbeiro " + getID());
+                    try {
+                        Thread.sleep(new Random().nextInt(2000) + 1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    barbearia.corteTerminado(cliente);
                 }
             }
         }
